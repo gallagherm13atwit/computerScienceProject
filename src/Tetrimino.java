dc492f9704965b30a80f37b45faf43281edaa60f
gallagherm13atwit <gallagherm13@wit.edu>
2020-04-10 12:06:10 -0400

    First implementation of Rotating Right!
    However, wall kicks and floor kicks need to be finalized before rotating
    left, as well as rotating line pieces. Methods have been made in
    Tetrimino to help handle wall and floor kicks.

--- src/Tetrimino.java
+++ src/Tetrimino.java
@@ -12,9 +12,17 @@ public class Tetrimino
 	ArrayList<Integer> sideBlocksRight = new ArrayList<Integer>();	//ArrayList<Integer> that holds the indexes of the tetrimino blocks that will collide with rectangles on the right side (if any are present)
 	int mostRight = 0;	//int that holds the current index of a rectangle that is closest to the right side of the screen given currX
 	int rotationPoint = 0;	//int that holds the index of the fourBlocks[] that doesn't move relative to rotation index
+	int oldRotPointVal = 0;
+	int rotWidth = 0; // Width of rotation of Tetrimino
+	int[] rotPointDiff = new int [4]; // Array that stores the location of the other 3 blocks in the Tetrimino surrounding the rotationPoint
 	int type = 0;	//int that holds the type of Tetrimino of value 0-6 (in order: Square, Line, T-Block, L-Block, Reverse L-Block, Z Block, and Reverse Z-Block)
 	Color colorBlock = Color.BLACK;	//Color object that stores the color of the Tetrimino (set during construction)
-	
+	int blockOrientation = 0; // what is the orientation of the block (default is 0)
+	int[] colisionBlocks;	// finds the blocks surrounding the Tetrimino that could colide with it if it were to rotate **UNLESS** its a line
+	int [] colisionBlocksLine; // finds the blocks surrounding the Tetrimino that could colide with it if it were to rotate **IF** its a line
+	int[] simRot = {0, 0, 0, 0};
+	int [] rotRules = {-11, -10, -9, 1, 11, 10, 9, -1}; //the location of the block in relation to the origin on the grid
+
 	/**
 	 * Constructor method that creates a Tetrimino object using
 	 * the given integers blockType and rotationWidth. Based on the value of
@@ -29,13 +37,15 @@ public class Tetrimino
 	public Tetrimino(int rotationWidth, int blockType)
 	{
 		type = blockType;
-		
+		rotWidth = rotationWidth;
+
 		if (rotationWidth == 2 && blockType == 0)	//Square
 		{
 			fourBlocks[0] = 4;
 			fourBlocks[1] = 5;
 			fourBlocks[2] = 14;
 			fourBlocks[3] = 15;
+			rotationPoint = 0;
 			colorBlock = Color.YELLOW;
 		}
 		else if (rotationWidth == 3 && blockType > 1)
@@ -46,6 +56,7 @@ public class Tetrimino
 				fourBlocks[1] = 13;
 				fourBlocks[2] = 14;
 				fourBlocks[3] = 15;
+				rotationPoint = 2;
 				colorBlock = Color.PURPLE;
 			}
 			else if (blockType == 3)	//L-Block
@@ -54,6 +65,7 @@ public class Tetrimino
 				fourBlocks[1] = 13;
 				fourBlocks[2] = 14;
 				fourBlocks[3] = 15;
+				rotationPoint = 2;
 				colorBlock = Color.ORANGE;
 			}
 			else if (blockType == 4)	//Reverse L-Block
@@ -62,6 +74,7 @@ public class Tetrimino
 				fourBlocks[1] = 13;
 				fourBlocks[2] = 14;
 				fourBlocks[3] = 15;
+				rotationPoint = 2;
 				colorBlock = Color.BLUE;
 			}
 			else if (blockType == 5)	//Z Block
@@ -70,6 +83,7 @@ public class Tetrimino
 				fourBlocks[1] = 4;
 				fourBlocks[2] = 14;
 				fourBlocks[3] = 15;
+				rotationPoint = 2;
 				colorBlock = Color.RED;
 			}
 			else if (blockType == 6)	//Reverse Z-Block
@@ -78,9 +92,9 @@ public class Tetrimino
 				fourBlocks[1] = 5;
 				fourBlocks[2] = 13;
 				fourBlocks[3] = 14;
+				rotationPoint = 3;
 				colorBlock = Color.LIGHTGREEN;
 			}
-			rotationPoint = 2;
 		}
 		else if (rotationWidth == 4 && blockType == 1)	//Line Block
 		{
@@ -88,21 +102,199 @@ public class Tetrimino
 			fourBlocks[1] = 4;
 			fourBlocks[2] = 5;
 			fourBlocks[3] = 6;
+			rotationPoint = 0;
 			colorBlock = Color.LIGHTBLUE;
-			
+
+		}
+		
+		oldRotPointVal = fourBlocks[rotationPoint];
+	}
+	
+	public void updateOrder()
+	{
+		boolean allCorrect = false;
+		
+		while (!allCorrect)
+		{
+			int prevVal = 0;
+			int tempVal = 0;
+			int order = 0;
+			for (int i = 1; i < 4; i++)
+			{
+				if (fourBlocks[i] < fourBlocks[prevVal])
+				{
+					tempVal = fourBlocks[i];
+					fourBlocks[i] = fourBlocks[prevVal];
+					fourBlocks[prevVal] = tempVal;
+				}
+				else
+					order++;
+				
+				prevVal++;
+			}
+			if (order == 3)
+			{
+				allCorrect = true;
+			}
+		}
+		
+		for (int j = 0; j < 4; j++)
+		{
+			if (fourBlocks[j] == oldRotPointVal)
+			{
+				rotationPoint = j;
+				oldRotPointVal = fourBlocks[j];
+			}
 		}
 	}
 	
+	public int[] simRotateRight()
+	{
+		simRot = this.getRotPointDiff();
+		int[] temp = this.getRotPointDiff();
+		for (int j = 0; j < 4; j++)
+		{
+			for (int i = 0; i < 8; i++)
+			{
+				if (j == rotationPoint)
+				{
+					simRot[j] = fourBlocks[j];
+				}
+				else if (temp[j] == rotRules[i])
+				{
+					simRot[j] = fourBlocks[rotationPoint] + rotRules[(i+2)%8];
+				}
+			}
+		}
+		
+		return simRot;
+	}
+	
+	public void rotateRight()
+	{
+		simRot = this.getRotPointDiff();
+		
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				if (simRot[j] == rotRules[i])
+				{
+					fourBlocks[j] = fourBlocks[rotationPoint] + rotRules[(i+2)%8];
+				}
+			}
+		}
+		this.changeOrientation(true);
+		this.updateOrder();
+	}
+	
+	/**
+	 * This is mainly going to be the same thing as simRotateRight
+	 * but instead of "rotRules[(i+2)%8]" it'll be
+	 * "rotRules[(i-2)%8] (like you said).
+	 */
+	public int[] simRotateLeft()
+	{
+		simRot = this.getRotPointDiff();
+		
+		return simRot;
+	}
+	
+	/**
+	 * This is mainly going to be the same thing as rotateRight
+	 * but instead of "rotRules[(i+2)%8]" it'll be
+	 * "rotRules[(i-2)%8] (like you said).
+	 */
+	public void rotateLeft()
+	{
+		this.changeOrientation(false);
+		this.updateOrder();
+	}
+	
+	/**
+	 * This is going to be called every time the rotation point
+	 * is up against a wall/block, hence the boolean wall parameter.
+	 * If it's true, then the wall/block will be on the RIGHT side
+	 * and result in the block being pushed left (aka, subtract 1 from the result).
+	 * If it's false, then the wall/block will be on the LEFT side and 
+	 * need to be pushed right (aka, add 1 to the result).
+	 * Rotation is needed to figure out which simRotate to call (that'll be
+	 * used to update simRot). You should only call this ONCE to see if it'll work.
+	 * If it doesn't then I'd try and repeat the process in TetrisGUI but
+	 * update the already existing int[] used for this call and try it then,
+	 * I don't think this'll work if you call it twice since the Tetrimino
+	 * values aren't being updated yet.
+	 * 
+	 * @param wall: tells if the wall/block next to the rotationPoint is on the right (true) or the left (false)
+	 * @param rotation: tells if the block is meant to be rotated right (true) or left (false)
+	 * @return a new simRot that will be a version of simRotate[Left/Right] pushed left or simRotate[Left/Right] pushed right
+	 */
+	public int[] simWallKick(boolean wall, boolean rotation)
+	{
+		simRot = this.getRotPointDiff();
+		
+		//determine if wall==true or wall==false 
+		//THEN if rotation==true or rotation==false
+		
+		return simRot;
+	}
+	
+	/**
+	 * This is going to be called every time the rotation point
+	 * is up against the floor. You'll need to call simRotateRight or simRotateLeft (hence the rotation boolean)
+	 * and return that result - 10. Just like simWallKick, this should really only be called once,
+	 * and if it doesn't work in TetrisGUI then it should be updated INSIDE TetrisGUI
+	 * (again, the Tetrimino values themselves aren't being updated, so it wouldn't work out).
+	 * 
+	 * @param rotation: true if the user is rotating right, false if rotating left
+	 * @return a new simRot that will subtract ten from all values
+	 */
+	public int[] simFloorKick(boolean rotation)
+	{
+		simRot = this.getRotPointDiff();
+		
+		return simRot;
+	}
+
 	//Returns the fourBlocks integer array
 	public int[] getBlocks()
 	{
 		return fourBlocks;
 	}
-	
+
 	//Returns colorBlock
 	public Color getColor()
 	{
 		return colorBlock;
+	}
+
+	//Returns the Type of Tetrimino;
+	public int getType()
+	{
+		return type;
+	}
+	
+	//Returns the RotationWidth of the Tetrimino
+	public int getRotWidth()
+	{
+		return rotWidth;
+	}
+	
+	//Returns the rotationPoint of the Tetrimino
+	public int getRotationPoint()
+	{
+		return rotationPoint;
+	}
+	
+	public int getRotPointIndex()
+	{
+		return fourBlocks[rotationPoint];
+	}
+	
+	//Returns the current orientation of the Tetrimino
+	public int getOrientation()
+	{
+		return blockOrientation;
 	}
 	
 	//Returns the value of fourBlocks[index]
@@ -110,13 +302,43 @@ public class Tetrimino
 	{
 		return fourBlocks[index];
 	}
-	
+
 	//Updates the values of fourBlocks[index] by valAdded
 	public void changeBlock(int index, int valAdded)
 	{
 		fourBlocks[index] += valAdded;
+		if (oldRotPointVal == fourBlocks[index] - valAdded)
+			oldRotPointVal = fourBlocks[index];
 	}
 	
+	
+	//Updates the Orientation of the current Tetrimino -- rotate clockwise if true, rotate counterclockwise if false
+	public void changeOrientation(boolean rotation)
+	{
+		if (rotation == true) { 
+			blockOrientation = (blockOrientation + 1) % 4;
+		} else { 
+			if (blockOrientation == 0) {
+				blockOrientation = 3;
+			} else {
+				blockOrientation = (blockOrientation - 1) % 4;
+			}
+		}
+	}
+	
+	public int[] getRotPointDiff() {
+		
+		for (int i = 0; i < 4; ++i) {
+			rotPointDiff[i] = fourBlocks[i] - fourBlocks[rotationPoint];
+		}
+		
+		return rotPointDiff;
+	}
+		
+	public void changeColisionBlocks() {
+		
+	}
+
 	/**
 	 * Returns bottomBlocks<Integer> after finding and
 	 * adding the Tetrimino's blocks which have no
@@ -128,9 +350,9 @@ public class Tetrimino
 	public ArrayList<Integer> bottomBlocks()
 	{
 		bottomBlocks.clear();	//clears the array to "freshen it up"
-		
+
 		bottomBlocks.add(fourBlocks[3]);
-		
+
 		if (type == 1 && fourBlocks[0]/10 == fourBlocks[3]/10)
 		{
 			bottomBlocks.add(fourBlocks[2]);
@@ -142,14 +364,14 @@ public class Tetrimino
 			for (int j = 2; j > -1; j--)
 			{
 				if (bottomBlocks.indexOf(fourBlocks[j] + 10) == -1
-				 && bottomBlocks.indexOf(fourBlocks[j] + 20) == -1)
-						bottomBlocks.add(fourBlocks[j]);
+						&& bottomBlocks.indexOf(fourBlocks[j] + 20) == -1)
+					bottomBlocks.add(fourBlocks[j]);
 			}
 		}
-		
+
 		return bottomBlocks;
-		}
-		
+	}
+
 	/**
 	 * Returs sideBlocksRight<Integer> after finding
 	 * and adding the Tetrimino's blocks (from top to bottom)
@@ -162,7 +384,7 @@ public class Tetrimino
 	public ArrayList<Integer> sideBlocksRight()
 	{
 		sideBlocksRight.clear();	//clear the array to "freshen it up"
-		
+
 		currX = fourBlocks[0]/10;	//evaluates currX to the first block
 		mostRight = fourBlocks[0];	//evaluates mostRight to the first block
 		for (int i = 1; i < 4; i++)	//for-loop that goes through each block starting from the 2nd block
@@ -177,10 +399,10 @@ public class Tetrimino
 			}
 		}
 		sideBlocksRight.add(mostRight);	//adds the final block to sideBlocksRight<Integer>
-		
+
 		return sideBlocksRight;	//returns sideBlocksRight<Integer>
 	}
-	
+
 	/**
 	 * Returns sideBlocksLeft<Integer> after finding
 	 * and adding the Tetrimino's blocks (from bottom to top)
@@ -193,7 +415,7 @@ public class Tetrimino
 	public ArrayList<Integer> sideBlocksLeft()
 	{
 		sideBlocksLeft.clear();	//clears the array to "freshen it up"
-		
+
 		currX = fourBlocks[3]/10;	//evaluates currX to the last block
 		mostLeft = fourBlocks[3];	//evaluates mostLeft to the last block
 		for (int i = 2; i > -1; i--)	//for-loop that goes through each block starting from the 3rd block
@@ -208,7 +430,7 @@ public class Tetrimino
 			}
 		}
 		sideBlocksLeft.add(mostLeft);	//adds the final block to sideBlocksLeft<Integer>
-		
+
 		return sideBlocksLeft;	//returns sideBlocksLeft<Integer>
 	}
 }
--- src/TetrisGUI.java
+++ src/TetrisGUI.java
@@ -31,8 +31,8 @@ import javafx.util.Duration;
  */
 public class TetrisGUI extends Application implements Initializable{
 
-	int speedInMilli = 1000;	//speed of blocks falling in milliseconds
-	
+	int speedInMilli = 750;	//speed of blocks falling in milliseconds
+
 	public static void main(String[] args)
 	{
 		launch(args);
@@ -42,34 +42,34 @@ public class TetrisGUI extends Application implements Initializable{
 	public void start(Stage primaryStage)
 	{
 		int sceneWidth = 500;	//holds the width of the scene and grid[]
-		int sceneHeight = 1000;	//holds the height of the scene and grid[]
+		int sceneHeight = 500;	//holds the height of the scene and grid[]
 		int numGrid = ((sceneWidth/50)*(sceneHeight/50) - 10);	//number of grid spaces
 		double currX = 0.0;	//current X value used to construct grid[]
 		double currY = 0.0;	//current Y value used to construct grid[]
 		int rotWidth = 0;	//holds the rotation width of a Tetrimino object
 		int randomBlockType = 0;	//creates the block type of a Tetrimino object
 		int[] currentBlocks = new int[4];	//holds the indexes of the currently controlled Tetrimino
-		
+
 		Rectangle[] grid = new Rectangle[numGrid];	//Rectangle array that creates the grid that shows the Tetrimino objects and allows the user to play
 		for (int c = 0; c < grid.length; c++)	//for-loop that initializes the Rectangles in grid[]
 		{
 			grid[c] = new Rectangle(50, 50);
 		}
-		
-		ArrayList<Tetrimino> blockInControl = new ArrayList<Tetrimino>();	//ArrayList that holds Tetrimino objects
-		for (int a = 0; a < 4; a++)	//for-loop that creates the first 4 Tetrimino objects within blockInControl
+
+		ArrayList<Tetrimino> tetInControl = new ArrayList<Tetrimino>();	//ArrayList that holds Tetrimino objects
+		for (int a = 0; a < 4; a++)	//for-loop that creates the first 4 Tetrimino objects within tetInControl
 		{
-			randomBlockType = (int) (Math.random()*7);	//uses Math.random() to store a value from 0-6 to randomBlockType
+			randomBlockType = (int) (Math.random()*5 + 2);	//uses Math.random() to store a value from 0-6 to randomBlockType
 			if (randomBlockType == 0)	//if-else statements that determines rotWidth based on randomBlockType
 				rotWidth = 2;
 			else if (randomBlockType == 1)
 				rotWidth = 4;
 			else if (randomBlockType > 1)
 				rotWidth = 3;
-			
-			blockInControl.add(new Tetrimino(rotWidth, randomBlockType));	//initializes a new tetrimino into index i of blockInControl
+
+			tetInControl.add(new Tetrimino(rotWidth, randomBlockType));	//initializes a new tetrimino into index i of tetInControl
 		}
-		
+
 		Pane p = new Pane();
 		for (int i = 0; i < grid.length; i++)	//for-loop that places the Rectangle in grid[] within the scene
 		{
@@ -86,39 +86,41 @@ public class TetrisGUI extends Application implements Initializable{
 			else
 				currX+=50.0;
 		}
-		
-		currentBlocks = blockInControl.get(0).getBlocks();	//initializes currentBlocks to the first Tetrimino object's indexes in blockInControl
-		
+
+		currentBlocks = tetInControl.get(0).getBlocks();	//initializes currentBlocks to the first Tetrimino object's indexes in tetInControl
+
 		for (int k = 0; k < 4; k++)	//adds the blocks from currentBlocks into the grid[] by filling the indexes with the Tetrimino's color
 		{
-			grid[currentBlocks[k]].setFill(blockInControl.get(0).getColor());
+			grid[currentBlocks[k]].setFill(tetInControl.get(0).getColor());
 		}
-	
+
 		//creates the scene
 		Scene scene = new Scene(p, sceneWidth, sceneHeight);
 		primaryStage.setTitle("Falling Block");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+
 		//EventHandler that is called every second to process the movement of Tetrimino's and when a new one is added into the scene
 		EventHandler<ActionEvent> time = new EventHandler<ActionEvent>()
 		{
-			int index = 0;	//index of blockInControl
-			Color currTetriminoColor = blockInControl.get(0).getColor();	//color of the current Tetrimino
-			ArrayList<Integer> bottomBlocks = blockInControl.get(0).bottomBlocks();	//Array List that stores the blocks at the bottom of the Tetrimino
-			ArrayList<Integer> leftBlocks = blockInControl.get(0).sideBlocksLeft();	//Array List that stores the blocks at the left-most side of the Tetrimino
-			ArrayList<Integer> rightBlocks = blockInControl.get(0).sideBlocksRight();	//Array List that stores the block at the right-most side of the Tetrimino
+			int index = 0;	//index of tetInControl
+			Color currTetriminoColor = tetInControl.get(0).getColor();	//color of the current Tetrimino
+			ArrayList<Integer> bottomBlocks = tetInControl.get(0).bottomBlocks();	//Array List that stores the blocks at the bottom of the Tetrimino
+			ArrayList<Integer> leftBlocks = tetInControl.get(0).sideBlocksLeft();	//Array List that stores the blocks at the left-most side of the Tetrimino
+			ArrayList<Integer> rightBlocks = tetInControl.get(0).sideBlocksRight();	//Array List that stores the block at the right-most side of the Tetrimino
 			boolean blocksAreAboveNothing = true;	//holds the number of blocks that from bottomBlocks that have no blocks immediately underneth them
 			int newRotWidth;	//holds the new rotation width of the newly created Tetrimino
 			int newRandomBlockType;	//creates the new Tetrimino type for the new Tetrimino
 			boolean cantMoveLeft = false;	//boolean status for if the tetrimino can move left
 			boolean cantMoveRight = false;	//boolean status for if the tetrimino can move right
-			
+			//boolean[] collisionBlocks = new boolean[8]; // detects true if surrounding grid blocks are not white
+			//int [] rotRules = {-11, -10, -9, 1, 11, 10, 9, -1}; //the location of the block in relation to the origin on the grid
+
 			@Override
 			public void handle(ActionEvent time)
 			{
-				if (index > 54)	//if statement that will remove old Tetrimino objects from blockInControl
-					blockInControl.remove(0);
+				if (index > 54)	//if statement that will remove old Tetrimino objects from tetInControl
+					tetInControl.remove(0);
 				
 				for (int a = 0; a < bottomBlocks.size(); a++)	//for-loop that checks for any blocks immediately below the indexes of bottomBlocks
 				{
@@ -130,53 +132,79 @@ public class TetrisGUI extends Application implements Initializable{
 					else
 						blocksAreAboveNothing = false;
 				}
-					
+
 				if (blocksAreAboveNothing)	//if statement that drops the blocks of Tetrimino into the next blocks below based on the value of blocksAboveNothing
 				{
 					for (int j = 3; j > -1; j--)	//for-loop that updates the indexes of the blocks in the current Tetrimino
 					{
-						grid[blockInControl.get(index).getBlockPlace(j)].setFill(Color.WHITE);	//makes the old index white
-						blockInControl.get(index).changeBlock(j, 10);
-						grid[blockInControl.get(index).getBlockPlace(j)].setFill(currTetriminoColor);	//makes the new index the color of the Tetrimino
+						grid[tetInControl.get(index).getBlockPlace(j)].setFill(Color.WHITE);	//makes the old index white
+						tetInControl.get(index).changeBlock(j, 10);
+						grid[tetInControl.get(index).getBlockPlace(j)].setFill(currTetriminoColor);	//makes the new index the color of the Tetrimino
 					}
-					bottomBlocks = blockInControl.get(index).bottomBlocks();	//updates bottomBlocks
-					leftBlocks = blockInControl.get(index).sideBlocksLeft();	//updates leftBlocks
-					rightBlocks = blockInControl.get(index).sideBlocksRight();	//updates rightBlocks
+					bottomBlocks = tetInControl.get(index).bottomBlocks();	//updates bottomBlocks
+					leftBlocks = tetInControl.get(index).sideBlocksLeft();	//updates leftBlocks
+					rightBlocks = tetInControl.get(index).sideBlocksRight();	//updates rightBlocks
 					blocksAreAboveNothing = true;	//updates blocksAboveNothing
 					cantMoveLeft = false;	//updates cantMoveLeft
-					cantMoveRight = false;	//updates cantMoveRight
+					cantMoveRight = false;	//updates cantMoveRight	
 				}
 				else	//else statement that activates if the Tetrimino is at the bottom of the screen or is immediately above another block
 				{
 					index++;	//increases index to the next Tetrimino
-					bottomBlocks = blockInControl.get(index).bottomBlocks();	//updates bottomBlocks to the bottomBlocks of the next Tetrimino
-					leftBlocks = blockInControl.get(index).sideBlocksLeft();	//updates leftBlockss to the sideBlocksLeft of the next Tetrimino
-					rightBlocks = blockInControl.get(index).sideBlocksRight();	//updates rightBlockss to the sideBlocksRight of the next Tetrimino
-					currTetriminoColor = blockInControl.get(index).getColor();	//updates the color of the indexes to that of the next Tetrimino
+					bottomBlocks = tetInControl.get(index).bottomBlocks();	//updates bottomBlocks to the bottomBlocks of the next Tetrimino
+					leftBlocks = tetInControl.get(index).sideBlocksLeft();	//updates leftBlockss to the sideBlocksLeft of the next Tetrimino
+					rightBlocks = tetInControl.get(index).sideBlocksRight();	//updates rightBlockss to the sideBlocksRight of the next Tetrimino
+					currTetriminoColor = tetInControl.get(index).getColor();	//updates the color of the indexes to that of the next Tetrimino
 					cantMoveLeft = false;	//updates cantMoveLeft
 					cantMoveRight = false;	//updates cantMoveRight
 					for (int b = 0; b < 4; b++)	//for-loop that creates the next Tetrimino on grid[]
 					{
-						grid[blockInControl.get(index).getBlockPlace(b)].setFill(currTetriminoColor);
+						grid[tetInControl.get(index).getBlockPlace(b)].setFill(currTetriminoColor);
 					}
 					blocksAreAboveNothing = true;
-				
-					//creates a new Tetrimino and adds it to blockInControl
-					newRandomBlockType = (int) (Math.random()*7);
+
+					//creates a new Tetrimino and adds it to tetInControl
+					newRandomBlockType = (int) (Math.random()*5 + 2);
 					if (newRandomBlockType == 0)
 						newRotWidth = 2;
 					else if (newRandomBlockType == 1)
 						newRotWidth = 4;
 					else if (newRandomBlockType > 1 && newRandomBlockType < 7)
 						newRotWidth = 3;
-					blockInControl.add(new Tetrimino(newRotWidth, newRandomBlockType));
+					tetInControl.add(new Tetrimino(newRotWidth, newRandomBlockType));
 				}
-				
-				
+
+
 				grid[0].setOnKeyPressed(new EventHandler<KeyEvent>(){
 					@Override
 					public void handle(KeyEvent event)
 					{
+						if (event.getCode() == KeyCode.UP) // rotate Tetrimino clockwise	
+						{
+							if (tetInControl.get(index).getType() > 1 && canRotate(true)) {
+								
+								for (int d = 0; d < 4; d++)
+									grid[tetInControl.get(index).getBlockPlace(d)].setFill(Color.WHITE);
+								
+								tetInControl.get(index).rotateRight();
+								
+								for (int i = 0; i < 4; i++)
+									grid[tetInControl.get(index).getBlockPlace(i)].setFill(currTetriminoColor);
+								
+								bottomBlocks = tetInControl.get(index).bottomBlocks();	//updates bottomBlocks
+								leftBlocks = tetInControl.get(index).sideBlocksLeft();	//updates leftBlocks
+								rightBlocks = tetInControl.get(index).sideBlocksRight();	//updates rightBlocks
+							}
+						}
+						
+						if (event.getCode() == KeyCode.DOWN)
+						{
+							if (tetInControl.get(index).getType() > 1 && canRotate(false))
+							{
+								//basically copy & paste the code from KeyCode.UP into this
+							}
+						}
+						
 						if (event.getCode() == KeyCode.LEFT)	//if statement for when the user wants to move the Tetrimino left
 						{
 							for (int i = 0; i < leftBlocks.size(); i++)	//for loop that sees if the Tetrimino can move left
@@ -192,16 +220,16 @@ public class TetrisGUI extends Application implements Initializable{
 							{
 								for (int d = 0; d < 4; d++)	//for loop that changes the index values within the Tetrimino object aand updates the grid
 								{
-									grid[blockInControl.get(index).getBlockPlace(d)].setFill(Color.WHITE);
-									blockInControl.get(index).changeBlock(d, -1);
-									grid[blockInControl.get(index).getBlockPlace(d)].setFill(currTetriminoColor);
+									grid[tetInControl.get(index).getBlockPlace(d)].setFill(Color.WHITE);
+									tetInControl.get(index).changeBlock(d, -1);
+									grid[tetInControl.get(index).getBlockPlace(d)].setFill(currTetriminoColor);
 								}
-								bottomBlocks = blockInControl.get(index).bottomBlocks();	//updates bottomBlocks
-								leftBlocks = blockInControl.get(index).sideBlocksLeft();	//updates leftBlocks
-								rightBlocks = blockInControl.get(index).sideBlocksRight();	//updates rightBlocks
+								bottomBlocks = tetInControl.get(index).bottomBlocks();	//updates bottomBlocks
+								leftBlocks = tetInControl.get(index).sideBlocksLeft();	//updates leftBlocks
+								rightBlocks = tetInControl.get(index).sideBlocksRight();	//updates rightBlocks
 							}
 						}
-						
+
 						if (event.getCode() == KeyCode.RIGHT)	//if statement for when the user wants to move the Tetrimino right
 						{
 							for (int i = 0; i < rightBlocks.size(); i++)	//for loop that sees if the Tetrimino can move right
@@ -217,29 +245,60 @@ public class TetrisGUI extends Application implements Initializable{
 							{
 								for (int d = 3; d > -1; d--)	//for loop that changes the index values for the Tetrimino object and updates the grid
 								{
-									grid[blockInControl.get(index).getBlockPlace(d)].setFill(Color.WHITE);
-									blockInControl.get(index).changeBlock(d, 1);
-									grid[blockInControl.get(index).getBlockPlace(d)].setFill(currTetriminoColor);
+									grid[tetInControl.get(index).getBlockPlace(d)].setFill(Color.WHITE);
+									tetInControl.get(index).changeBlock(d, 1);
+									grid[tetInControl.get(index).getBlockPlace(d)].setFill(currTetriminoColor);
 								}
-								bottomBlocks = blockInControl.get(index).bottomBlocks();
-								leftBlocks = blockInControl.get(index).sideBlocksLeft();
-								rightBlocks = blockInControl.get(index).sideBlocksRight();
+								bottomBlocks = tetInControl.get(index).bottomBlocks();
+								leftBlocks = tetInControl.get(index).sideBlocksLeft();
+								rightBlocks = tetInControl.get(index).sideBlocksRight();
 							}
 						}
 					}
 				});
 			}
+			
+			public boolean canRotate(boolean rOrL)	//true = right; false = left
+			{
+				if (rOrL)
+				{
+					int[] simRot = tetInControl.get(index).simRotateRight();
+					
+					for (int k = 0; k < 4; k++)
+					{
+						grid[tetInControl.get(index).getBlockPlace(k)].setFill(Color.BLACK);
+					}
+				
+					for (int i = 0; i < 4; i++)
+					{
+						if (!grid[simRot[i]].getFill().equals(Color.WHITE) && !grid[simRot[i]].getFill().equals(Color.BLACK)/*ADD A CONDITION: && kickedFloor == 0*/)
+						{
+							for (int j = 0; j < 4; j++)
+							{
+								grid[tetInControl.get(index).getBlockPlace(j)].setFill(currTetriminoColor);
+							}
+							return false;
+						}
+					}
+				}
+				else
+				{
+					//same thing as above but lefty
+				}
+				
+				return true;
+			}
 		};
-		
+
 		Timeline a = new Timeline(new KeyFrame(Duration.millis(speedInMilli), time));	//plays the EventHandler every second
 		a.setCycleCount(Timeline.INDEFINITE);
 		grid[0].requestFocus();	//request control for the first Rectangle of grid[] [THIS INDEX DOE NOT MOVE, IT ONLY RECEIVES INSTRUCTIONS]
 		a.play();
 	}
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
